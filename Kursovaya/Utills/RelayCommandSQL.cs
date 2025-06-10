6cd8280847924cd54bfc764b246880d96396bf13
popovas <popovas@209-9>
2025-06-10 17:57:14 +0300

    Добавьте файлы проекта.

---
+++ Kursovaya/Utills/RelayCommandSQL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Kursovaya.Utills
+{
+    public class RelayCommandSQL : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action DoWork;
+        public RelayCommandSQL(Action work)
+        {
+            DoWork = work;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            DoWork();
+        }
+    }
+}
---
+++ Kursovaya/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Kursovaya.Utills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Kursovaya.ViewModel
+{
+    public class NavigationViewModel : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+        public ICommand HomeCommand { get; set; }
+        public ICommand EmployeeCommand { get; set; }
+
+        private void Home(object obj) => CurrentView = new HomeViewModel();
+        private void EmployeePage(object obj) => CurrentView = new EmployeeViewModel();
+        public NavigationViewModel()
+        {
+            HomeCommand = new RelayCommand(Home);
+            EmployeeCommand = new RelayCommand(EmployeePage);
+            // Startup Page
+            CurrentView = new HomeViewModel();
+        }
+    }
+}
